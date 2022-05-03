c588d1ca2f7ff89e28530841169d6cb17011ff2a
seydang <53308177+seydang@users.noreply.github.com>
2022-05-03 11:50:42 -0400

    Update sound_effects.py

--- sound_effects.py
+++ sound_effects.py
@@ -25,7 +25,29 @@ def pitch_shifter(RATE, x, fr = 20, shift = 5):
 
     return ns    
         
+    
+def darth_vader(RATE, x, speed_factor, delay, low_freq):
+    # speed
+    num = len(x)
+    sound_index = np.round(np.arange(0, num, speed_factor))
+	slow_x = x[sound_index[sound_index < num].astype(int)]
+    
+    # echo
+    echo_x = np.zeros(num)
+    output_delay = delay * RATE
 
+    for count, e in enumerate(slow_x):
+        echo_x[count] = e + slow_x[count - int(output_delay)]
+        
+        
+    # lowpass
+    nyquist = RATE / 2.0
+    cutoff = low_freq / nyquist
+    x, y = signal.butter(order=4, cutoff, btype='lowpass', analog=False)
+    low_x = signal.filtfilt(x, y, echo_x)
+    
+    return low_x
+    
 # def robotic(RATE, index, m, fc = 2000):
 #     mh = hilbert(m)
 #     carrier_signal = [ A*sin(2*pi*fc*n/RATE) for n in range(index, index+1024)]
