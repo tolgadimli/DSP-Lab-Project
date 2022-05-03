94effcd83c7edb30c9960c2d76f11bc9836aa3ad
seydang <53308177+seydang@users.noreply.github.com>
2022-05-03 11:43:35 -0400

    Update sound_effects.py

--- sound_effects.py
+++ sound_effects.py
@@ -10,6 +10,22 @@ def amplitude_mod(RATE, index, x, fc = 2000, A = 5000, phase = 0 ):
     y = (1+x_arr)*carrier_signal
     return y
 
+
+def pitch_shifter(RATE, x, fr = 20, shift = 5):
+    fr = 20         # A larger number for fr means less reverb.
+    sz = RATE//fr   # Read and process 1/fr second at a time.
+    shift = 100//fr # shifting 100 Hz
+    x_arr = np.asarray(x)
+     
+    lf = np.fft.rfft(x, 100)
+    lf = np.roll(lf, shift)
+    lf[0:shift] = 0
+    nl = np.fft.irfft(lf)
+    ns = np.column_stack((nl)).ravel().astype(np.int16)
+
+    return ns    
+        
+
 # def robotic(RATE, index, m, fc = 2000):
 #     mh = hilbert(m)
 #     carrier_signal = [ A*sin(2*pi*fc*n/RATE) for n in range(index, index+1024)]
