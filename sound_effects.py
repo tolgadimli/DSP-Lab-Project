f878cfaa4256d0c9d1e7111fec9205650f450959
Tolga Dimlioğlu <67735246+tolgadimli@users.noreply.github.com>
2022-05-05 17:07:56 -0400

    Add files via upload

--- sound_effects.py
+++ sound_effects.py
@@ -2,15 +2,42 @@ from math import sin, cos, pi
 import numpy as np
 import scipy.signal as signal
 from scipy.signal import hilbert
+import struct
 
-def amplitude_mod(RATE, index, x, fc = 2000, A = 5000, phase = 0 ):
+def amplitude_mod(fs, index, x, fc = 2000, A = 5000, phase = 0 ):
 
+    length = len(x)
     x_arr = np.asarray(x)
-    carrier_signal = [ A*sin(2*pi*fc*n/RATE + phase) for n in range(index, index+1024)]
+    carrier_signal = [ A*sin(2*pi*fc*n/fs + phase) for n in range(index, index+length)]
     carrier_signal = np.asarray(carrier_signal)
     y = (1+x_arr)*carrier_signal
-    return y
 
+    a, b = signal.butter(4, 0.5, btype='lowpass', analog=False)
+    y_low = signal.filtfilt(a, b, y)
+
+    return y_low
+
+def robotic(fs, index, m, fc = 2000):
+
+    length = len(m)
+    m_arr = np.asarray(m)
+
+    mh = hilbert(m)
+    # print(length)
+    # print(type(mh))
+    
+
+    carrier_signal_sin = np.array([ sin(2*pi*fc*n/fs) for n in range(index, index+length)])
+    carrier_signal_cos = np.array([ cos(2*pi*fc*n/fs) for n in range(index, index+length)])
+
+    sbu = 2 * m_arr *  carrier_signal_cos - np.real(mh * 2 * carrier_signal_sin)
+    sbl = 2 * m_arr *  carrier_signal_cos + np.real(mh * 2 * carrier_signal_sin)
+
+    y = sbu + m_arr + sbl
+    a, b = signal.butter(4, 0.5, btype='lowpass', analog=False)
+    y_low = signal.filtfilt(a, b, y)
+
+    return y_low
 
 def pitch_shifter(RATE, index, x, shift = 5):
     sz = 2*1024   # Read and process 1/fr second at a time.
@@ -61,8 +88,18 @@ def baby(RATE, start_index, x, speed_factor=2.0, freq=200):
 
     return low_x
     
-# def robotic(RATE, index, m, fc = 2000):
-#     mh = hilbert(m)
-#     carrier_signal = [ A*sin(2*pi*fc*n/RATE) for n in range(index, index+1024)]
 
-#     sbu = 2 * m *  
+def add_sound(x, block_ind, wf, wave_length, wave_width):
+    #x_arr = np.asarray(x)
+    L = len(x)
+    if (block_ind + 1) * L < wave_length:
+        wf.setpos(block_ind * L)
+    else:
+        wf.setpos(0) 
+        
+    sound_portion_bin = wf.readframes(L)
+    sound_portion = struct.unpack('h' * L, sound_portion_bin) 
+    sound_portion_arr = np.asarray(sound_portion)
+    y = x + sound_portion_arr
+
+    return y
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import wave
+
 def clip16( x ):    
     # Clipping for 16 bits
     if x > 32767:
@@ -7,3 +9,36 @@ def clip16( x ):
     else:
         x = x        
     return(x)
+
+
+def get_harbour_wave():
+    wavfile = 'sea.wav'
+    wf = wave.open( wavfile, 'rb' )
+    signal_length   = wf.getnframes()       # Signal length
+    width           = wf.getsampwidth()     # Sample width
+
+    return wf, signal_length, width
+
+def get_park_wave():
+    wavfile = 'park.wav'
+    wf = wave.open( wavfile, 'rb' )
+    signal_length   = wf.getnframes()       # Signal length
+    width           = wf.getsampwidth()     # Sample width
+
+    return wf, signal_length, width
+
+def get_starwars_wave():
+    wavfile = 'star_wars.wav'
+    wf = wave.open( wavfile, 'rb' )
+    signal_length   = wf.getnframes()       # Signal length
+    width           = wf.getsampwidth()     # Sample width
+
+    return wf, signal_length, width
+
+def get_space_wave():
+    wavfile = 'space.wav'
+    wf = wave.open( wavfile, 'rb' )
+    signal_length   = wf.getnframes()       # Signal length
+    width           = wf.getsampwidth()     # Sample width
+
+    return wf, signal_length, width
